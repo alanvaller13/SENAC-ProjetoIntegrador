7c93cd10d9b7cbb10331fbd5fa1fc89861530f2e
Alan <alan.valler@alunos.sc.senac.br>
2023-12-06 20:16:09 -0300

    css da pagina da postagem

--- detalhes_noticia.php
+++ detalhes_noticia.php
@@ -1,4 +1,235 @@
-<?php 
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <title>Portal SENAC NEWS - Seu Ponto de Informação</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="icon" href="IMG/Logo.png">
+  <link rel="stylesheet" href="CSS/style-cards.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+  <style>
+    .container {
+      display: flex;
+      gap: 2px;
+      width: 100%;
+      flex-wrap: wrap;
+      text-align: center;
+    }
+
+    h1 {
+      font-size: 15x;
+      text-align: center;
+    }
+
+    h3 {
+      color: #FFF;
+      margin-top: -10px;
+      font-size: 17px;
+      text-align: center;
+    }
+
+    .box {
+      display: flex !important;
+      align-items: center !important;
+      justify-content: center !important;
+      flex-grow: 1 !important;
+      flex-basis: 100px !important;
+    }
+
+    .boximg {
+      height: 75%;
+      width: 75%;
+      border-radius: 8px;
+    }
+
+ .postagem h2{
+    text-align: center;
+    font-size: 50px;
+    font-family: arial;
+    border: 10px;
+    word-break: break-all;
+            font-family: arial;
+ }
+
+.postagem h6{
+    text-align: center;
+    font-size: 17px;
+    word-break: break-all;
+            font-family: arial;
+    }
+    .postagem h4{
+        font: 23px;
+        padding-left: 10px;
+        padding-right: 10px;
+        line-height: 1.5;
+        letter-spacing: 1px;
+        word-break: break-all;
+        margin: 75px;
+        }
+
+        .reportagem {
+            font: 40px;
+            padding-left: 10px;
+            padding-right: 10px;
+            line-height: 1.5;
+            letter-spacing: 1px;
+            word-break: break-all;
+            font-family: arial;
+            text-align: justify;
+  text-justify: inter-word;
+  margin: 75px;
+        }
+
+    .box2 {
+      width: 45%;
+      border-radius: 8px;
+      align-items: center;
+    }
+
+    .boximg2 {
+      max-width: 500px;
+      border-radius: 8px;
+      align-items: center;
+    }
+    
+
+  </style>
+
+</head>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+
+  <a class="navbar-brand" href="index.html"><img src="IMG/Logo.png" width="40" height="40"></a>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="javascript:void(0)" style="color: #ffffff;">Política</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="javascript:void(0)" style="color: #ffffff;">Entretenimento</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="javascript:void(0)" style="color: #ffffff;">Esporte</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="javascript:void(0)" style="color: #ffffff;">Tempo</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="javascript:void(0)" style="color: #ffffff;">Tecnologia</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Contato/Contato.html" style="color: #ffffff;">Contatos</a>
+      </li>
+    </ul>
+    <div class="text-center" style="margin-bottom:0">
+
+    </div>
+  </div>
+  <a class="nav-link" href="PaginaLogin.html"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25"
+      fill="currentColor" class="bi bi-person-fill-lock" viewBox="0 0 16 16">
+      <path
+        d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-9 8c0 1 1 1 1 1h5v-1a1.9 1.9 0 0 1 .01-.2 4.49 4.49 0 0 1 1.534-3.693C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4m7 0a1 1 0 0 1 1-1v-1a2 2 0 1 1 4 0v1a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1zm3-3a1 1 0 0 0-1 1v1h2v-1a1 1 0 0 0-1-1" />
+    </svg></a>
+  <form class="d-flex">
+
+    <input class="form-control me-2" type="text" placeholder="Pesquisar">
+    <button class="btn btn-primary" type="button">
+      <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-search"
+        viewBox="0 0 16 16">
+        <path
+          d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+      </svg>
+    </button>
+  </form>
+</nav>
+<header>
+  <div id="demo" class="carousel slide" data-ride="carousel">
+
+    <ul class="carousel-indicators">
+      <li data-target="#demo" data-slide-to="0" class="active"></li>
+      <li data-target="#demo" data-slide-to="1"></li>
+
+    </ul>
+
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <a href="https://www.gsuplementos.com.br/" target="_blank"><img src="IMG/growth.webp" width="100%" height="300%"
+            alt="Growth"></a>
+      </div>
+      <div class="carousel-item">
+        <a href="https://joacaba.sc.gov.br/" target="_blank"><img src="IMG/joaçaba.jpg" width="100%" height="300%"
+            alt="Joaçaba"></a>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+    </a>
+    <a class="carousel-control-next" href="#demo" data-slide="next">
+      <span class="carousel-control-next-icon"></span>
+    </a>
+  </div>
+
+  <br>
+
+  <section class="container">
+    <section class="box">
+      <div>
+        <img class="boximg" src="IMG/artworks-aS7wofDIdqq3LqF6-GGirXg-t500x500.jpg">
+      </div>
+    </section>
+    <section class="box">
+      <div>
+        <img class="boximg" src="IMG/images.jpg">
+      </div>
+    </section>
+    <section class="box">
+      <div>
+        <img class="boximg" src="IMG/images.jpg">
+      </div>
+    </section>
+    <section class="box">
+      <div>
+        <img class="boximg" src="IMG/images.jpg">
+      </div>
+    </section>
+    <section class="box">
+      <div>
+        <img class="boximg" src="IMG/images.jpg">
+      </div>
+    </section>
+    <section class="box">
+      <div>
+        <img class="boximg" src="IMG/images.jpg">
+      </div>
+    </section>
+    <section class="box">
+      <div>
+        <img class="boximg" src="IMG/images.jpg">
+      </div>
+    </section>
+    <section class="box">
+      <div>
+        <img class="boximg" src="IMG/images.jpg">
+      </div>
+    </section>
+
+  </section>
+</header>
+
+<br>
+
+<body>
+    <div class="postagem">
+    <h3> Notícias</h3>
+    <div class="news">
+    <?php 
     require 'controller/conexao.php';
 
     $noticia_id = isset($_GET['id']) ? $_GET['id'] : 0;  
@@ -12,10 +243,10 @@ if ($result->num_rows > 0) {
 
     $row = $result->fetch_assoc();
     echo "<h2>{$row['titulo']}</h2>";
-    echo "<p>Tag: {$row['tag']}</p>";
-    echo "<p>Data da Reportagem: {$row['data_reportagem']}</p>";
-    echo "<p>Descrição: {$row['descricao']}</p>";
-    echo "<p>Reportagem: {$row['reportagem']}</p>";
+    echo "<h6>Tag: {$row['tag']}</h6>";
+    echo "<h6>Data da Reportagem: {$row['data_reportagem']}</h6>";
+    echo "<h4>Descrição: {$row['descricao']}</h4>";
+    echo "<p class=\"reportagem\" >Reportagem: {$row['reportagem']}</p>";
 } else {
     echo "Notícia não encontrada.";
 }
@@ -23,3 +254,42 @@ if ($result->num_rows > 0) {
 
 $conn->close();
 ?>
+
+  </div>
+
+
+  <section class="container">
+    <section class="box">
+      <div>
+        <img class="box" 
+        style="  width: 100%;" 
+        src="IMG/images2.jpg">
+      </div>
+    </section>
+
+    <section class="box">
+      <div>
+        <img class="box" 
+        style="width: 100%;" 
+        src="IMG/images2.jpg">
+      </div>
+    </section>
+  </section>
+
+  <br>
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</body>
+
+<footer>
+<div class="jumbotron text-center">
+  <p class="footer">Todos os Direitos Reservados - 2023 - Projeto Integrador - Senac </p>
+    <p class="footer">Criado por Alan, Jonas, Jean, Leonardo, Arthur, Carlos </p>
+</div>
+<footer>
+
+
+
+</html>
